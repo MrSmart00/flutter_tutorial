b8b078c7fb8beb604f7496e15e355d6d37aa7f3d
Hiroya Hinomori <nexearth@gmail.com>
2020-10-12 21:26:14 +0900

    Add: complete tutorial project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# flutter_tutorial
-Tutorial project with Flutter
+# first_flutter_app
+
+https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/index.html?index=..%2F..index#0
+https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2/index.html?index=..%2F..index#0
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:first_flutter_app/random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),                         // ... to here.
+      home: RandomWords()
+    );
+  }
+}
+
